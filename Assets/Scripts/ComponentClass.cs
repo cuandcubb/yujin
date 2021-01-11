a11079df615fa7cd4d95e9fb9e9a9dc43c74477e
문유진 <76810113+cuandcubb@users.noreply.github.com>
2021-01-11 18:48:24 +0900

    Initial commit

---
+++ Assets/Scripts/ComponentClass.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentClass
+{
+    public string name;                 //이름
+    public Vector2 position;            //부위의 위치
+    public Sprite unSproutedSprite;     //꽃이 피어나지 않았을 때 스프라이트 이미지
+    public Sprite sproutedSprite;       //꽃이 피어났을 때의 스프라이트 이미지
+    public GameObject realGameobject;   //이 컴포넌트가 가진 실제의 게임오브젝트
+    public float sproutTime;            //피어나는 데 걸리는 시간
+    public SpriteRenderer spriteRenderer;
+    //스프라이트 렌더러 컴포넌트, 나중에 realGameObject.GetComponent<SpriteRenderer>()해야 함
+
+    public ComponentClass()
+    {
+        position = Vector2.zero;
+
+        sproutTime = 10;
+    }
+}
+
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //문제점: 오브젝트가 같은 위치에 겹쳐서 생성됨, A를 연타하면 마지막에 생성된 것만 스프라이트가 바뀜 나머지는 안 바뀜
+    //bodyplantproject에 만들어두신거 참고하면서 해봤습니다
+    
+    List<ComponentClass> componentList = new List<ComponentClass>();
+
+    public GameObject imageObject;
+
+    public Sprite unspro;
+    public Sprite spro;
+
+    public int i;
+
+    public void Sprouting()
+    {
+        componentList.Add(new ComponentClass());
+
+        GameObject obj = Instantiate(imageObject);
+        componentList[i].realGameobject = obj;
+
+        componentList[i].spriteRenderer = obj.GetComponent<SpriteRenderer>();
+
+        componentList[i].unSproutedSprite = unspro;
+        componentList[i].sproutedSprite = spro;
+
+        componentList[i].spriteRenderer.sprite = componentList[i].unSproutedSprite;
+        StartCoroutine(SproutingCoroutine());
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Sprouting();
+        }
+    }
+    
+    IEnumerator SproutingCoroutine()
+    {
+        yield return new WaitForSeconds(componentList[i].sproutTime);
+        componentList[i].spriteRenderer.sprite = componentList[i].sproutedSprite;
+    }
+}
